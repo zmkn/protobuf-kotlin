3355d7d2d70e3c4e28b79e9deb433db22d235595
hz <beijingren@vip.qq.com>
2025-01-10 16:11:41 +0800

    调整 gradle 编译配置

--- stub/build.gradle.kts
+++ stub/build.gradle.kts
@@ -6,20 +6,8 @@ protobuf {
     protoc {
         artifact = libs.protobuf.protoc.get().toString()
     }
-    plugins {
-        create("grpc") {
-            artifact = libs.grpc.protoc.gen.grpc.java.get().toString()
-        }
-        create("grpckt") {
-            artifact = libs.grpc.protoc.gen.grpc.kotlin.get().toString() + ":jdk8@jar"
-        }
-    }
     generateProtoTasks {
         all().forEach {
-            it.plugins {
-                create("grpc")
-                create("grpckt")
-            }
             it.builtins {
                 create("kotlin")
             }
@@ -31,9 +19,6 @@ dependencies {
     protobuf(project(":protobuf"))
     api(libs.protobuf.java.util)
     api(libs.protobuf.kotlin)
-    api(libs.grpc.stub)
-    api(libs.grpc.protobuf)
-    api(libs.grpc.kotlin.stub)
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
