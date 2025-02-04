89bf2e73c1685f05088a4b21dc20b47fa4bee1e0
hz <beijingren@vip.qq.com>
2025-02-04 22:53:35 +0800

    调整 Gradle 配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,9 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://repository.zmkn.com/repository/maven-public/")
         }
-        maven { url = uri("https://jitpack.io") }
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "protobuf-kotlin"
